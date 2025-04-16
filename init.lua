e4d93abf486c5f075ec4d1e24891c88a6d01465c
ywattenberg <wattenberg@mailbox.org>
2025-04-16 22:35:44 +0200

    update

--- init.lua
+++ init.lua
@@ -950,6 +950,12 @@ require('lazy').setup({
         return '%2l:%-2v'
       end
 
+      require('mini.sessions').setup()
+      require('mini.move').setup()
+      -- require('mini.operators').setup()
+      -- require('mini.snippets').setup()
+      --
+
       -- ... and there is more!
       --  Check out: https://github.com/echasnovski/mini.nvim
     end,
@@ -1000,7 +1006,7 @@ require('lazy').setup({
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
@@ -1027,6 +1033,10 @@ require('lazy').setup({
     },
   },
 })
+
+---- START OF CUSTOM CONFIG SETTINGS
+
+vim.opt.conceallevel = 1
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
---
+++ lua/custom/plugins/dashboard.lua
@@ -0,0 +1,33 @@
+return {
+  'nvimdev/dashboard-nvim',
+  event = 'VimEnter',
+  config = function()
+    require('dashboard').setup {
+      -- config
+      theme = 'hyper',
+      config = {
+        week_header = {
+          enable = true,
+        },
+        shortcut = {
+          { desc = '󰊳 Update', group = '@property', action = 'Lazy update', key = 'u' },
+          {
+            icon = ' ',
+            icon_hl = '@variable',
+            desc = 'Files',
+            group = 'Label',
+            action = 'Telescope find_files',
+            key = 'f',
+          },
+          {
+            desc = " ToDo's",
+            group = 'Label',
+            action = 'edit ~/Nextcloud/Obsidian/Personal/Todo.md',
+            key = 'a',
+          },
+        },
+      },
+    }
+  end,
+  dependencies = { { 'nvim-tree/nvim-web-devicons' } },
+}
